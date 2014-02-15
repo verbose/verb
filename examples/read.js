84d6e8d922f42aa0321c848c77dbd59e711d15ea
Jon Schlinkert <dev@sellside.com>
2014-02-15 15:52:29 -0500

    adds "read" example

---
+++ examples/read.js
@@ -0,0 +1,10 @@
+/**
+ * phaser <https://github.com/jonschlinkert/phaser>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, contributors.
+ * Licensed under the MIT license.
+ */
+
+var phaser = require('../');
+console.log(phaser);
+phaser.file.read('README.tmpl.md');
