0d2c70a281803335e3c59d9274fa770a4fcf92f0
Jon Schlinkert <dev@sellside.com>
2014-02-16 20:13:34 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * phaser <https://github.com/assemble/phaser>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+module.exports = 'stun or kill?';
