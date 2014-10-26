5722042db3672b42e39e0e63552844ffd8279f21
jonschlinkert <dev@sellside.com>
2014-10-26 11:24:36 -0400

    first commit

---
+++ bin/cli.js
@@ -0,0 +1,216 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var path = require('path');
+var gutil = require('gulp-util');
+var prettyTime = require('pretty-hrtime');
+var chalk = require('chalk');
+var semver = require('semver');
+var archy = require('archy');
+var Liftoff = require('liftoff');
+var tildify = require('tildify');
+var interpret = require('interpret');
+var v8flags = require('v8flags');
+var completion = require('../lib/completion');
+var argv = require('minimist')(process.argv.slice(2));
+var taskTree = require('../lib/taskTree');
+
+// set env var for ORIGINAL cwd
+// before anything touches it
+process.env.INIT_CWD = process.cwd();
+
+var cli = new Liftoff({
+  name: 'verb',
+  completions: completion,
+  extensions: interpret.jsVariants,
+  nodeFlags: v8flags.fetch()
+});
+
+// exit with 0 or 1
+var failed = false;
+process.once('exit', function(code) {
+  if (code === 0 && failed) {
+    process.exit(1);
+  }
+});
+
+// parse those args m8
+var cliPackage = require('../package');
+var versionFlag = argv.v || argv.version;
+var tasksFlag = argv.T || argv.tasks;
+var tasks = argv._;
+var toRun = tasks.length ? tasks : ['default'];
+
+// this is a hold-over until we have a better logging system
+// with log levels
+var simpleTasksFlag = argv['tasks-simple'];
+var shouldLog = !argv.silent && !simpleTasksFlag;
+
+if (!shouldLog) {
+  gutil.log = function(){};
+}
+
+cli.on('require', function (name) {
+  gutil.log('Requiring external module', chalk.magenta(name));
+});
+
+cli.on('requireFail', function (name) {
+  gutil.log(chalk.red('Failed to load external module'), chalk.magenta(name));
+});
+
+cli.on('respawn', function (flags, child) {
+  var nodeFlags = chalk.magenta(flags.join(', '));
+  var pid = chalk.magenta(child.pid);
+  gutil.log('Node flags detected:', nodeFlags);
+  gutil.log('Respawned to PID:', pid);
+});
+
+cli.launch({
+  cwd: argv.cwd,
+  configPath: argv.verbfile,
+  require: argv.require,
+  completion: argv.completion
+}, handleArguments);
+
+
+// the actual logic
+function handleArguments(env) {
+  if (versionFlag && tasks.length === 0) {
+    gutil.log('CLI version', cliPackage.version);
+    if (env.modulePackage && typeof env.modulePackage.version !== 'undefined') {
+      gutil.log('Local version', env.modulePackage.version);
+    }
+    process.exit(0);
+  }
+
+  env.modulePath = path.resolve(__dirname, '../index.js');
+  if (!env.modulePath) {
+    gutil.log(
+      chalk.red('Local verb not found in'),
+      chalk.magenta(tildify(env.cwd))
+    );
+    gutil.log(chalk.red('Try running: npm install verb'));
+    process.exit(1);
+  }
+
+  if (!env.configPath) {
+    gutil.log(chalk.red('No verbfile found'));
+    process.exit(1);
+  }
+
+  // check for semver difference between cli and local installation
+  if (semver.gt(cliPackage.version, env.modulePackage.version)) {
+    gutil.log(chalk.red('Warning: verb version mismatch:'));
+    gutil.log(chalk.red('Global verb is', cliPackage.version));
+    gutil.log(chalk.red('Local verb is', env.modulePackage.version));
+  }
+
+  // chdir before requiring verbfile to make sure
+  // we let them chdir as needed
+  if (process.cwd() !== env.cwd) {
+    process.chdir(env.cwd);
+    gutil.log(
+      'Working directory changed to',
+      chalk.magenta(tildify(env.cwd))
+    );
+  }
+
+  // this is what actually loads up the verbfile
+  require(env.configPath);
+  gutil.log('Using verbfile', chalk.magenta(tildify(env.configPath)));
+
+  var verbInst = require(env.modulePath);
+  logEvents(verbInst);
+
+  process.nextTick(function () {
+    if (simpleTasksFlag) {
+      return logTasksSimple(env, verbInst);
+    }
+    if (tasksFlag) {
+      return logTasks(env, verbInst);
+    }
+    verbInst.start.apply(verbInst, toRun);
+  });
+}
+
+function logTasks(env, localGulp) {
+  var tree = taskTree(localGulp.tasks);
+  tree.label = 'Tasks for ' + chalk.magenta(tildify(env.configPath));
+  archy(tree)
+    .split('\n')
+    .forEach(function (v) {
+      if (v.trim().length === 0) {
+        return;
+      }
+      gutil.log(v);
+    });
+}
+
+function logTasksSimple(env, localGulp) {
+  console.log(Object.keys(localGulp.tasks)
+    .join('\n')
+    .trim());
+}
+
+// format orchestrator errors
+function formatError(e) {
+  if (!e.err) {
+    return e.message;
+  }
+
+  // PluginError
+  if (typeof e.err.showStack === 'boolean') {
+    return e.err.toString();
+  }
+
+  // normal error
+  if (e.err.stack) {
+    return e.err.stack;
+  }
+
+  // unknown (string, number, etc.)
+  return new Error(String(e.err)).stack;
+}
+
+// wire up logging events
+function logEvents(verbInst) {
+
+  // total hack due to poor error management in orchestrator
+  verbInst.on('err', function () {
+    failed = true;
+  });
+
+  verbInst.on('task_start', function (e) {
+    // TODO: batch these
+    // so when 5 tasks start at once it only logs one time with all 5
+    gutil.log('Starting', '\'' + chalk.cyan(e.task) + '\'...');
+  });
+
+  verbInst.on('task_stop', function (e) {
+    var time = prettyTime(e.hrDuration);
+    gutil.log(
+      'Finished', '\'' + chalk.cyan(e.task) + '\'',
+      'after', chalk.magenta(time)
+    );
+  });
+
+  verbInst.on('task_err', function (e) {
+    var msg = formatError(e);
+    var time = prettyTime(e.hrDuration);
+    gutil.log(
+      '\'' + chalk.cyan(e.task) + '\'',
+      chalk.red('errored after'),
+      chalk.magenta(time)
+    );
+    gutil.log(msg);
+  });
+
+  verbInst.on('task_not_found', function (err) {
+    gutil.log(
+      chalk.red('Task \'' + err.task + '\' is not in your verbfile')
+    );
+    gutil.log('Please check the documentation for proper verbfile formatting');
+    process.exit(1);
+  });
+}
---
+++ index.js
@@ -0,0 +1,303 @@
+/*!
+ * verb <https://github.com/assemble/verb>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+// process.env.DEBUG = 'verb';
+
+var util = require('util');
+var vfs = require('vinyl-fs');
+var File = require('gulp-util').File;
+var es = require('event-stream');
+var debug = require('debug')('verb');
+var Engine = require('engine');
+var Config = require('orchestrator');
+var parser = require('parser-front-matter');
+var stack = require('./lib/defaults');
+var utils = require('./lib/utils');
+var _ = require('lodash');
+var extend = _.extend;
+
+
+/**
+ * Create an instance of `Verb` with the given `options`.
+ *
+ * ```js
+ * var verb = new Verb();
+ * ```
+ *
+ * @param {Object} `options`
+ * @constructor
+ * @api public
+ */
+
+var Verb = module.exports = Engine.extend({
+  constructor: function (options) {
+    Verb.__super__.constructor.call(this, options);
+    this._defaultConfig();
+    this._defaultRoutes();
+    this._defaultTemplates();
+    Config.call(this);
+  }
+});
+
+util.inherits(Verb, Engine);
+extend(Verb.prototype, Config.prototype);
+
+/**
+ * Initialize default template types
+ *
+ * @api private
+ */
+
+Verb.prototype._defaultConfig = function() {
+  this.disable('built-in:engines');
+
+  this.option('viewEngine', '.md');
+  this.option('destExt', '.md');
+  this.option('defaults', {
+    isRenderable: true,
+    isPartial: true,
+    // vinyl: true,
+    engine: '.md',
+    ext: '.md'
+  })
+};
+
+/**
+ * Initialize default template types
+ *
+ * @api private
+ */
+
+Verb.prototype.defaultPlugins = function() {
+  this.enable('src:routes');
+  this.enable('src:extend');
+  this.enable('dest:path');
+  this.enable('dest:extend');
+  this.enable('dest:routes');
+  this.enable('dest:render');
+};
+
+/**
+ * Initialize default template types
+ *
+ * @api private
+ */
+
+Verb.prototype._defaultTemplates = function() {
+  this.create('include', this.option('defaults'));
+  this.create('file', this.option('defaults'));
+  this.create('doc', this.option('defaults'));
+};
+
+/**
+ * Load default middleware
+ *
+ *   - `.md`: parse front matter in markdown files
+ *   - `.hbs`: parse front matter in handlebars templates
+ *
+ * @api private
+ */
+
+Verb.prototype._defaultRoutes = function() {
+  this.route(/\.*$/, function (src, dest, next) {
+    debug('default route: %j', arguments);
+    parser.parse(unBuffer(src), function (err) {
+      if (err) return next(err);
+      next();
+    });
+  });
+};
+
+/**
+ * Register default template delimiters.
+ *
+ *   - engine delimiters: Delimiters used in templates process by [engine-lodash], the default engine.
+ *   - layout delimiters: Delimiters used in layouts.
+ *
+ * @api private
+ */
+
+Verb.prototype.defaultDelimiters = function() {
+  this.addDelims('md', ['{%', '%}'], ['<<%', '%>>']);
+};
+
+/**
+ * Define a Verb task.
+ *
+ * ```js
+ * verb.task('docs', function() {
+ *   // do stuff
+ * });
+ * ```
+ *
+ * @param {String} `name`
+ * @param {Function} `fn`
+ * @api public
+ */
+
+Verb.prototype.task = Verb.prototype.add;
+
+/**
+ * Run an array of tasks.
+ *
+ * ```js
+ * verb.run(['foo', 'bar']);
+ * ```
+ *
+ * @param {Array} `tasks`
+ * @api public
+ */
+
+Verb.prototype.run = function () {
+  var tasks = arguments.length ? arguments : ['default'];
+  this.start.apply(this, tasks);
+};
+
+/**
+ * Glob patterns or filepaths to source files.
+ *
+ * ```js
+ * verb.task('site', function() {
+ *   verb.src('src/*.hbs', {layout: 'default'})
+ *     verb.dest('dist')
+ * });
+ * ```
+ *
+ * @param {String|Array} `glob`
+ * @param {Object} `options`
+ * @api public
+ */
+
+Verb.prototype.src = function (glob, options) {
+  if (this.enabled('minimal')) {
+    return vfs.dest(glob, options);
+  }
+
+  debug('src: %j', arguments);
+
+  return es.pipe.apply(es, utils.arrayify([
+    vfs.src(glob, options),
+    stack.src(this, glob, options)
+  ]));
+};
+
+/**
+ * Specify a destination for processed files.
+ *
+ * ```js
+ * verb.task('sitemap', function() {
+ *   verb.src('src/*.txt')
+ *     verb.dest('dist', {ext: '.xml'})
+ * });
+ * ```
+ * @param {String|Array|Function} `patterns` Glob patterns, file paths, or renaming function.
+ * @param {Object} `opts` Options to be passed to `dest` plugins.
+ * @api public
+ */
+
+Verb.prototype.dest = function (dest, options) {
+  if (this.enabled('minimal')) {
+    return vfs.dest(dest, options);
+  }
+
+  debug('dest: %j', arguments);
+
+  return es.pipe.apply(es, utils.arrayify([
+    stack.dest(this, dest, options),
+    vfs.dest(dest, options)
+  ]));
+};
+
+/**
+ * Rerun the specified task when a file changes.
+ *
+ * ```js
+ * verb.task('watch', function() {
+ *   verb.watch('docs/*.md', ['docs']);
+ * });
+ * ```
+ *
+ * @param  {String|Array} `glob` Filepaths or glob patterns.
+ * @param  {String} `options`
+ * @param  {Function} `fn` Task(s) to watch.
+ * @api public
+ */
+
+Verb.prototype.watch = function (glob, opts, fn) {
+  if (Array.isArray(opts) || typeof opts === 'function') {
+    fn = opts;
+    opts = null;
+  }
+
+  if (Array.isArray(fn)) {
+    return vfs.watch(glob, opts, function () {
+      this.start.apply(this, fn);
+    }.bind(this));
+  }
+  return vfs.watch(glob, opts, fn);
+};
+
+/**
+ * Un-buffer the contents of a template.
+ *
+ * @api private
+ */
+
+function unBuffer(value) {
+  if (value == null) {
+    return {};
+  }
+
+  if (!Buffer.isBuffer(value.contents)) {
+    return value;
+  }
+
+  debug('unBuffer: %j', arguments);
+  value.content = value.contents.toString();
+  var o = {};
+
+  for (var key in value) {
+    if (['contents', '_contents'].indexOf(key) === -1) {
+      o[key] = value[key];
+    }
+  }
+  return o;
+}
+
+/**
+ * When `options.vinyl` is true, transform the value to
+ * a vinyl file.
+ *
+ * @param  {Object} `value`
+ * @return {Object} Returns a vinyl file.
+ * @api private
+ */
+
+function toVinyl(value) {
+  debug('toVinyl: %j', arguments);
+
+  return new File({
+    contents: new Buffer(value.content),
+    path: value.path,
+    locals: value.locals || {},
+    options: value.options || {}
+  });
+}
+
+/**
+ * Expose `verb.Verb`
+ */
+
+Verb.prototype.Verb = Verb;
+
+/**
+ * Expose `verb`
+ */
+
+module.exports = new Verb();
