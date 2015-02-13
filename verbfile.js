86f34bc68ea88ff9751322e21f983a390e7a9fef
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-13 10:55:01 -0500

    adds istanbul/mocha

--- verbfile.js
+++ verbfile.js
@@ -1,17 +1,33 @@
 'use strict';
 
+var istanbul = require('gulp-istanbul');
+var jshint = require('gulp-jshint');
+var mocha = require('gulp-mocha');
 var verb = require('./');
 
 // verb.layout('foo.md', 'AAA\n<<% body %>>');
 
 verb.task('readme', function() {
-  verb.src('.verb*.md')
-    .pipe(verb.dest('.'));
+  verb.src('.verb.md')
+    .pipe(verb.dest('./'));
 });
 
-verb.task('docs', function() {
-  verb.src('docs/.verb/*.md')
-    .pipe(verb.dest('docs'));
+verb.task('lint', function () {
+  verb.src(['index.js', 'lib/**/*.js'])
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
-verb.task('default', ['readme', 'docs']);
+verb.task('test', function (cb) {
+  verb.src(['index.js', 'lib/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+    .on('finish', function () {
+      verb.src('test/*.js')
+        .pipe(mocha())
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
+});
+
+verb.task('default', ['lint', 'test', 'readme', 'api-docs']);
