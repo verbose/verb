c77c77f509f35f7a9af715d355b928cd5e67cb3a
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-22 06:41:38 -0400

    rename

--- test/helpers.collections.js
+++ test/helpers.collections.js
@@ -11,10 +11,18 @@ var verb = require('..');
 require('should');
 
 describe('loaded helpers', function () {
-  it('should use helpers from a module dependency:', function (done) {
+  it('should use helpers from the template-helpers library:', function (done) {
     verb.render('{%= add(1, 2) %}', function (err, content) {
       if (err) console.log(err);
       content.should.equal('3');
+      done();
+    });
+  });
+
+  it('should use helpers from the markdown-utils library:', function (done) {
+    verb.render('{%= mdu.link("a", "b") %}', function (err, content) {
+      if (err) console.log(err);
+      content.should.equal('[a](b)');
       done();
     });
   });
