6cec9e7351527cb4281f236d64a85bf9819aed44
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-14 14:19:54 -0400

    add tasks for pulling down assemble tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
+'use strict';
+
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var stylish = require('jshint-stylish');
 var istanbul = require('gulp-istanbul');
 var jshint = require('gulp-jshint');
-require('jshint-stylish');
+var mocha = require('gulp-mocha');
+var git = require('gulp-git');
+var del = require('rimraf');
 
-var lint = ['index.js', 'lib/*.js'];
+var lint = ['index.js', 'lib/{*,plugins/*}.js'];
+
+function url(repo) {
+  return 'https://github.com/' + repo;
+}
 
 gulp.task('coverage', function () {
   return gulp.src(lint)
@@ -12,19 +20,37 @@ gulp.task('coverage', function () {
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('mocha', ['coverage'], function () {
+gulp.task('test', ['clone', 'coverage'], function () {
   return gulp.src('test/*.js')
     .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports())
     .pipe(istanbul.writeReports({
       reporters: [ 'text' ],
       reportOpts: {dir: 'coverage', file: 'summary.txt'}
-    }))
+    }));
 });
 
-gulp.task('jshint', function () {
-  return gulp.src(lint)
+gulp.task('lint', function () {
+  return gulp.src(lint.concat('test/*.js'))
     .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('default', ['mocha', 'jshint']);
+gulp.task('spec', ['clone'], function (cb) {
+  gulp.src('test/_spec/test/*.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .on('end', function () {
+      del('test/_spec', cb);
+    });
+});
+
+gulp.task('clone', function(cb) {
+  del('test/_spec', function (err) {
+    if (err) return cb(err);
+    git.clone(url('assemble/assemble'), {
+      args: 'test/_spec'
+    }, cb);
+  });
+});
+
+gulp.task('default', ['test', 'lint']);
