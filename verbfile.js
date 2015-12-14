2401e7feb0dd6b54fa2606cd4f268f7588a305b2
jonschlinkert <jon.schlinkert@sellside.com>
2015-12-13 21:57:41 -0500

    start cleaning up verbfile

--- verbfile.js
+++ verbfile.js
@@ -2,10 +2,18 @@
 
 var path = require('path');
 var utils = require('./lib/utils');
-var defaults = require('./lib/runner/defaults');
+var data = require('./lib/runner/data');
 var argv = require('minimist')(process.argv.slice(2), {
-  alias: {v: 'verbose'}
+  alias: {
+    v: 'verbose'
+  }
 });
+
+/**
+ * !HEADS UP: this is not what a normal verfile.js would look like.
+ * We're just trying to break everyting right now, so this has a
+ * lot of stuff in it.
+ */
 
 module.exports = function(verb, base, env) {
   if (argv.init) {
@@ -14,43 +22,42 @@ module.exports = function(verb, base, env) {
 
   var tasks = verb.get('env.argv.tasks') || ['readme', 'package'];
 
+  verb.task('a', function(cb) {
+    console.log('verb base > a');
+    cb();
+  });
+  verb.task('b', function(cb) {
+    console.log('verb base > b');
+    cb();
+  });
+  verb.task('c', function(cb) {
+    console.log('verb base > c');
+    cb();
+  });
+
   /**
    * Readme task
    */
 
   verb.task('readme', function(cb) {
-    verb.questions.enqueue('author', 'name', 'description');
-
     var plugins = verb.get('env.argv.plugins') || verb.plugins;
-    var pkg = env.user.pkg;
-    var config = pkg.verb || {};
-
-    verb.option(config.options || {});
-
-    // load package.json data and user options onto `verb.cache.data`
-    verb.data({options: verb.options});
-    verb.data(pkg);
-    verb.data(verb.base.get('cache.expanded'));
-    verb.data({license: license(pkg, verb.options)});
+    data.updateData(verb, verb.base, verb.env);
 
     // ask pre-configured questions, but only if
     // they don't have answers yet
     verb.ask(function(err, answers) {
       if (err) return cb(err);
 
-      // placeholder for something better
-      verb.emit('info', 'plugins',  Object.keys(plugins).join(', '));
-
-      verb.toStream('docs', function(key) {
-          return key === '.verb';
-        })
-        .pipe(handle('onStream'))
+      verb.toStream('docs', function(key, view) {
+        return key === '.verb';
+      })
+        .pipe(handle(verb, 'onStream'))
         .pipe(verb.renderFile('text', answers))
         .on('error', handleError(verb))
         .pipe(verb.pipeline(plugins))
-        .pipe(handle('preWrite'))
+        .pipe(handle(verb, 'preWrite'))
         .pipe(verb.dest(dest()))
-        .pipe(utils.exhaust(handle('postWrite')))
+        .pipe(utils.exhaust(handle(verb, 'postWrite')))
         .on('error', cb)
         .on('finish', cb);
     });
@@ -58,58 +65,27 @@ module.exports = function(verb, base, env) {
 
   verb.task('package', function(cb) {
     verb.toStream('docs', function(key, view) {
-        return view.basename === 'package.json';
-      })
-      .pipe(handle('preWrite'))
+      return view.basename === 'package.json';
+    })
+      .pipe(handle(verb, 'preWrite'))
       .pipe(verb.dest(dest()))
-      .pipe(utils.exhaust(handle('postWrite')))
+      .pipe(utils.exhaust(handle(verb, 'postWrite')))
       .on('error', cb)
       .on('finish', cb);
   });
 
-  verb.task('verbmd', function(cb) {
-    verb.ask(function(err, answers) {
-      if (err) return cb(err);
-
-      verb.toStream('docs', function(key) {
-          return key === 'min';
-        })
-        .pipe(verb.dest(dest('.verb.md')))
-        .on('error', cb)
-        .on('finish', cb);
-    });
-  });
-
-  verb.task('docs', function(cb) {
-    verb.ask(function(err, answers) {
-      if (err) return cb(err);
-
-      verb.toStream('docs')
-        .on('error', cb)
-        .pipe(verb.renderFile('text', answers))
-        .on('error', cb)
-        .pipe(verb.dest(dest()))
-        .on('finish', cb);
-    });
-  });
-
-  verb.register('store', function(app, base) {
-    app.task('del', function(cb) {
-      verb.store.del({force: true});
-      console.log('deleted store.');
-      cb();
-    });
-  });
-
   verb.task('default', tasks);
-
-  function handle(stage) {
-    return utils.through.obj(function(file, enc, next) {
-      if (file.isNull()) return next();
-      verb.handle(stage, file, next);
-    });
-  }
 };
+
+function handle(app, stage) {
+  return utils.through.obj(function(file, enc, next) {
+    if (file.isNull()) return next();
+    if (typeof app.handle !== 'function') {
+      return next(null, file);
+    }
+    app.handle(stage, file, next);
+  });
+}
 
 /**
  * Rename template files
@@ -127,25 +103,6 @@ function dest(dest) {
 }
 
 /**
- * Format license
- */
-
-function license(pkg, options) {
-  options = options || {};
-  if (typeof options.license === 'string') {
-    return options.license;
-  }
-  var str = pkg.license;
-  if (Array.isArray(pkg.licenses)) {
-    str = pkg.licenses[0];
-  }
-  if (typeof str === 'undefined') {
-    return '';
-  }
-  return 'Released under the ' + str + ' license.';
-}
-
-/**
  * Handle render errors
  */
 
@@ -154,9 +111,7 @@ function handleError(app) {
     var m = /(\w+) is not a function/.exec(err.message);
     var msg = '';
     if (m) {
-      msg = err.message + ': "' + m[1] + '()" is defined as a helper\n'
-       + 'in `.verb.md`, but "' + m[1] + '" is defined on '
-       + 'verb.cache.data as a "' + typeof app.cache.data[m[1]] + '"';
+      msg = err.message + ': "' + m[1] + '()" is defined as a helper\n' + 'in `.verb.md`, but "' + m[1] + '" is defined on ' + 'verb.cache.data as a "' + typeof app.cache.data[m[1]] + '"';
     }
     if (app.options.verbose) {
       console.log(msg);
@@ -166,5 +121,5 @@ function handleError(app) {
       console.log(msg);
     }
     process.exit(1);
-  }
+  };
 }
