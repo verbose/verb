0ba65686df408c034b3466008eebbedcd3bd8aa5
jonschlinkert <jon.schlinkert@sellside.com>
2016-01-11 18:51:49 -0500

    fix error messages in applyLayout tests

--- test/app.applyLayout.js
+++ test/app.applyLayout.js
@@ -12,72 +12,72 @@ var page = {
   }
 };
 
-describe('helpers', function () {
-  describe('rendering', function () {
-    beforeEach(function () {
+describe('helpers', function() {
+  describe('rendering', function() {
+    beforeEach(function() {
       app = new App();
       app.engine('tmpl', require('engine-base'));
       app.create('layout', {viewType: 'layout'});
       app.create('page');
     });
 
-    it('should throw an error when a layout cannot be found:', function (done) {
+    it('should throw an error when a layout cannot be found:', function(cb) {
       app.layout('fofof.tmpl', {content: '..'});
       app.page('a.tmpl', page)
-        .render(function (err) {
-          assert(err.message === 'Templates#layouts no layouts are registered, but one is defined: default.tmpl');
-          done();
+        .render(function(err) {
+          assert(/layouts/.test(err.message));
+          cb();
         });
     });
 
-    it('should emit an error when a layout cannot be found:', function (done) {
+    it('should emit an error when a layout cannot be found:', function(cb) {
       app.layout('fofof.tmpl', {content: '..'});
-      app.on('error', function (err) {
-        assert(err.message === 'Templates#layouts no layouts are registered, but one is defined: default.tmpl');
-        done();
+      app.on('error', function(err) {
+        assert(/layouts/.test(err.message));
+        cb();
       });
 
       app.page('a.tmpl', page)
-        .render(function () {
+        .render(function() {
         });
     });
 
-    it('should throw an error - layout defined but no layouts registered:', function (done) {
+    it('should throw an error - layout defined but no layouts registered:', function(cb) {
       app.page('a.tmpl', page)
-        .render(function (err) {
-          assert(err.message === 'Templates#layouts no layouts are registered, but one is defined: default.tmpl');
-          done();
+        .render(function(err) {
+          assert(/layouts/.test(err.message));
+          cb();
         });
     });
 
-    it('should emit an error - layout defined but no layouts registered:', function (done) {
-      app.on('error', function (err) {
-        assert(err.message === 'Templates#layouts no layouts are registered, but one is defined: default.tmpl');
-        done();
+    it('should emit an error - layout defined but no layouts registered:', function(cb) {
+      app.on('error', function(err) {
+        assert(/layouts/.test(err.message));
+        cb();
       });
       app.page('a.tmpl', page)
-        .render(function () {
+        .render(function() {
         });
     });
 
-    it('should wrap a view with a layout (view.render):', function (done) {
+    it('should wrap a view with a layout (view.render):', function(cb) {
       app.layout('default.tmpl', {content: 'before {% body %} after'});
       app.page('a.tmpl', page)
-        .render(function (err) {
-          if (err) return done(err);
-          done();
+        .render(function(err) {
+          if (err) return cb(err);
+          cb();
         });
     });
 
-    it('should wrap a view with a layout (app.render):', function (done) {
+    it('should wrap a view with a layout (app.render):', function(cb) {
       app.layout('default.tmpl', {content: 'before {% body %} after'});
       app.page('a.tmpl', page);
 
       var view = app.pages.getView('a.tmpl');
-      app.render(view, function (err, res) {
-        if (err) return done(err);
-        assert(res.contents.toString() === 'before Halle after');
-        done();
+      app.render(view, function(err, res) {
+        if (err) return cb(err);
+        assert.equal(res.contents.toString(), 'before Halle after');
+        cb();
       });
     });
   });
