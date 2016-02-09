682ac844f852f92c7d69a6ec8f8a8f6a48254a9a
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-09 16:49:57 -0500

    update verbfile to use readme generator

--- verbfile.js
+++ verbfile.js
@@ -1,8 +1,12 @@
 'use strict';
 
-module.exports = function custom(app) {
-  app.task('foo', function(cb) {
+module.exports = function(verb) {
+  verb.extendWith('readme');
+
+  verb.task('foo', function(cb) {
     console.log('verb > verbfile >', this.name, 'task');
     cb();
   });
+
+  verb.task('default', ['readme']);
 };
