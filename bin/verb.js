2c0331910bc5ecb30c10e059e67ccf87a0edf2b4
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-09 01:10:10 -0500

    simplify cli

--- bin/verb.js
+++ bin/verb.js
@@ -1,32 +1,24 @@
 #!/usr/bin/env node
 
 process.env.VERB_CLI = true;
-var runtimes = require('base-runtimes');
 var Verb = require('..');
 var verb = new Verb();
 
-// register default "fallback" generator
-verb.register('fallback', require('../lib/generators/default'));
-verb.register('generator', require('../lib/generators/generator'));
-verb.register('verbfile', require('../lib/generators/verbfile'));
-verb.register('verbmd', require('../lib/generators/verbmd'));
-
 // run generator and/or tasks
-verb.runner('verbfile.js', function(err, argv, app) {
+verb.runner('verbfile.js', function(err, opts, app) {
   if (err) {
     console.error(err.message);
     process.exit(1);
   }
 
-  app.use(runtimes(config));
+  if (!app.hasConfigfile) {
+    app.register('default', require('../lib/generators/default'));
+  }
 
-  var config = app.loadSettings(argv);
-  app.set('cache.config', config);
-
-  app.config.process(config, function(err) {
+  app.config.process(opts, function(err) {
     if (err) throw err;
 
-    app.cli.process(argv, function(err) {
+    app.cli.process(opts, function(err) {
       if (err) throw err;
 
       verb.emit('done');
