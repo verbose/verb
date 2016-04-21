12bc4dcece3df0d12722bf7c273df8976b68517d
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-21 04:46:28 -0400

    simply

--- index.js
+++ index.js
@@ -7,10 +7,7 @@
 
 'use strict';
 
-var utils = require('generator-util');
-var define = require('define-property');
 var Generate = require('generate');
-var pkg = require('./package');
 
 /**
  * Create a verb application with `options`.
@@ -27,19 +24,9 @@ function Verb(options) {
   if (!(this instanceof Verb)) {
     return new Verb(options);
   }
-
-  this.options = utils.extend({}, this.options, options);
-  Generate.call(this, this.options);
-  delete this.cache.data.runner;
-
+  Generate.call(this, options);
   this.is('verb');
-  Generate.debug(this);
-
-  Verb.emit('preInit', this, this.base);
-  this.debug('initializing');
-  this.verbDefaults(options);
   this.initVerb(this.options);
-  Verb.emit('init', this, this.base);
 }
 
 /**
@@ -49,43 +36,20 @@ function Verb(options) {
 Generate.extend(Verb);
 
 /**
- * Initialize verb defaults
- */
-
-Verb.prototype.verbDefaults = function(options) {
-  this.debug('initializing verb defaults');
-  var defaults = { prefix: 'verb', configfile: 'verbfile.js' };
-  this.options = utils.extend(defaults, this.options, options);
-};
-
-/**
  * Initialize verb data
  */
 
 Verb.prototype.initVerb = function(opts) {
-  this.debug('initializing verb data');
+  this.debug('initializing', __filename);
 
-  this.set('appname', 'verb');
-  this.data({runner: {name: 'verb', url: 'https://github.com/verbose/verb'}});
-  var aliasRegex = /^verb-([^-]+)-generator$/g;
-
-  this.toFullname = function(name) {
-    return 'verb-' + this.toAlias(name) + '-generator';
-  };
-
-  this.isGeneratorPath = function(fp) {
-    return aliasRegex.test(fp) || /^generate-/.test(fp);
-  };
-
-  this.toAlias = function(name) {
-    return name.replace(aliasRegex, '$1');
-  };
-
-  // if run via CLI, add defaults
-  if (process.env.GENERATE_CLI) {
-    this.use(utils.create(this.options));
-    this.create('files');
+  delete this.cache.data.runner;
+  if (this.options.namespace === true) {
+    delete this.options.namespace;
   }
+
+  Verb.emit('preInit', this, this.base);
+  this.data({runner: require('./package')});
+  Verb.emit('init', this, this.base);
 };
 
 /**
