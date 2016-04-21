b0f6dc30cb10160c6f3e245bb1d5a08c4da6ed5f
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-21 12:02:27 -0400

    update patterns, lint gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,60 +4,25 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var eslint = require('gulp-eslint');
-var through = require('through2');
+
+var lib = ['index.js', 'lib/**/*.js', 'bin/*.js'];
 
 gulp.task('coverage', function() {
-  return gulp.src(['index.js', 'lib/*.js'])
-    .pipe(istanbul({includeUntested: true}))
+  return gulp.src(lib)
+    .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('mocha', ['coverage'], function() {
+gulp.task('test', ['coverage'], function() {
   return gulp.src('test/*.js')
-    .pipe(mocha())
+    .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports());
 });
 
-gulp.task('eslint', function() {
-  return gulp.src(['*.js', 'lib/*.js', 'test/*.js'])
+gulp.task('lint', function() {
+  return gulp.src(['*.js', 'test/*.js'].concat(lib))
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
-gulp.task('vars', function() {
-  var utils = require('./lib/utils');
-  var keys = Object.keys(utils);
-  var report = {};
-  var cache = {};
-
-  return gulp.src(['index.js', 'lib/**/*.js', 'bin/*.'])
-    .pipe(through.obj(function(file, enc, next) {
-      var str = file.contents.toString();
-      keys.forEach(function(key) {
-        report[key] = report[key] || 0;
-        var re = cache[key] || (cache[key] = new RegExp('\\.' + key, 'g'));
-        var m = str.match(re);
-        if (!m) return;
-        report[key]++;
-      });
-
-      next(null, file);
-    }, function(next) {
-      var keys = Object.keys(report);
-      var res = {};
-
-      keys.sort(function(a, b) {
-        return report[a] > report[b] ? -1 : 1;
-      });
-
-      keys.forEach(function(key) {
-        res[key] = report[key];
-      });
-
-      console.log(res);
-      console.log(keys.length + 1, 'modules');
-      next();
-    }))
-});
-
-gulp.task('default', ['mocha', 'eslint']);
+gulp.task('default', ['test', 'lint']);
