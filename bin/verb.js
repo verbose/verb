90129e815082e148236a88e0ac34cd44b23af554
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-21 04:46:41 -0400

    use base-runner

--- bin/verb.js
+++ bin/verb.js
@@ -1,24 +1,100 @@
 #!/usr/bin/env node
 
 process.env.GENERATE_CLI = true;
-var verb = require('..');
+
+var argv = require('minimist')(process.argv.slice(2));
+var runner = require('base-runner');
+var commands = require('../lib/commands');
+var Verb = require('..');
+
+// lift-off options
+var options = {
+  name: 'verb',
+  runner: require('../package'),
+  lookup: lookup
+};
 
 /**
- * Create verb "runner"
+ * Initialize verb CLI
  */
 
-var cli = verb.runner('verbfile.js', require('../lib/generator'));
-var app = verb();
+runner(Verb, options, argv, function(err, app, runnerContext) {
+  if (err) app.handleError(err);
 
-/**
- * Run generators and tasks
- */
+  /**
+   * Register `verb` generator and tasks
+   */
 
-cli(app, function(err, argv, app) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
+  app.register('verb', require('../lib/generator'));
+
+  /**
+   * Load custom commands
+   */
+
+  commands(app, runnerContext);
+
+  // get the config object from package.json
+  var config = app.pkg.get(runnerContext.env.name);
+  var args = app.argv(runnerContext.argv);
+
+  // set parsed and unparsed argv on `cache`
+  app.set('cache.argv', {
+    orig: argv,
+    parsed: runnerContext.argv,
+    processed: args
+  });
+
+  if (config && !args.noconfig) {
+    app.set('cache.config', config);
   }
-  app.emit('done');
-  process.exit();
+
+  app.option('runner.new', 'files');
+  app.option(argv);
+
+  /**
+   * Custom `lookup` function, for resolving generators
+   */
+
+  app.option('lookup', lookup(app));
+
+  /**
+   * Process argv
+   */
+
+  if (args.init === true) {
+    app.cli.process({init: true}, function(err, obj) {
+      if (err) app.emit('error', err);
+      delete obj.init;
+      run(app, obj);
+    });
+  } else {
+    run(app, args);
+  }
 });
+
+/**
+ * cli process
+ */
+
+function run(app, args) {
+  app.cli.process(args, function(err, obj) {
+    if (err) app.emit('error', err);
+
+    app.emit('done');
+    process.exit();
+  });
+}
+
+/**
+ * Custom lookup function for resolving generators
+ */
+
+function lookup(app) {
+  return function(key) {
+    var patterns = [`verb-${key}-generator`, key];
+    if (app.enabled('generators')) {
+      patterns.push(`generate-${key}`);
+    }
+    return patterns;
+  }
+}
