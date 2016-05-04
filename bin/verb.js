08c5df54010491996082297b87344d6ab6db0cd4
jonschlinkert <jon.schlinkert@sellside.com>
2016-05-03 22:27:49 -0400

    reflect changes from base-runner

--- bin/verb.js
+++ bin/verb.js
@@ -2,88 +2,37 @@
 
 process.env.GENERATE_CLI = true;
 
-var argv = require('minimist')(process.argv.slice(2));
-var runner = require('base-runner');
+var generator =  require('../lib/generator');
 var commands = require('../lib/commands');
+var utils = require('../lib/utils');
+var argv = utils.yargs(process.argv.slice(2));
 var Verb = require('..');
-
-// lift-off options
-var options = {
-  name: 'verb',
-  runner: require('../package'),
-  lookup: lookup
-};
 
 /**
  * Initialize verb CLI
  */
 
-runner(Verb, options, argv, function(err, app, runnerContext) {
-  if (err) app.handleError(err);
+utils.runner(Verb, {name: 'verb'}, argv, function(err, app, ctx) {
+  if (err) {
+    console.log(err.stack);
+    process.exit(1);
+  }
 
-  /**
-   * Register `verb` generator and tasks
-   */
-
-  app.register('verb', require('../lib/generator'));
-
-  /**
-   * Load custom commands
-   */
-
-  commands(app, runnerContext);
-
-  // get the config object from package.json
-  var config = app.pkg.get(runnerContext.env.name);
-  var args = app.argv(runnerContext.argv);
-
-  // set parsed and unparsed argv on `cache`
-  app.set('cache.argv', {
-    orig: argv,
-    parsed: runnerContext.argv,
-    processed: args
+  app.on('error', function(err) {
+    console.log(err.stack);
+    process.exit(1);
   });
 
-  if (config && !args.noconfig) {
-    app.set('cache.config', config);
-  }
-
-  app.option('runner.new', 'files');
-  app.option(argv);
-
-  /**
-   * Custom `lookup` function, for resolving generators
-   */
-
+  commands(app, ctx);
+  app.register('verb', require('../lib/generator'));
   app.option('lookup', lookup(app));
 
-  /**
-   * Process argv
-   */
-
-  if (args.init === true) {
-    app.cli.process({init: true}, function(err, obj) {
-      if (err) app.emit('error', err);
-      delete obj.init;
-      run(app, obj);
-    });
-  } else {
-    run(app, args);
-  }
-});
-
-/**
- * cli process
- */
-
-function run(app, args) {
-  app.cli.process(args, function(err, obj) {
+  app.cli.process(ctx.argv, function(err) {
     if (err) app.emit('error', err);
-
     app.emit('done');
     process.exit();
   });
-}
+});
 
 /**
  * Custom lookup function for resolving generators
