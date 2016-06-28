363690c1d1f68942c9b264bc7939e5c6e2c09c62
jonschlinkert <jon.schlinkert@sellside.com>
2016-06-28 14:46:58 -0400

    adds macro support, better task handling

--- bin/verb.js
+++ bin/verb.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
 process.env.GENERATE_CLI = true;
+require('set-blocking')(true);
 
-var generator =  require('../lib/generator');
-var commands = require('../lib/commands');
-var utils = require('../lib/utils');
-var argv = utils.yargs(process.argv.slice(2));
+var util = require('util');
+var path = require('path');
 var Verb = require('..');
+var commands = require('../lib/commands');
+var tasks = require('../lib/tasks');
+var utils = require('../lib/utils');
+var args = process.argv.slice(2);
+var argv = require('yargs-parser')(args);
+if (argv.v) {
+  console.log('generate v' + Verb.pkg.version);
+  process.exit();
+}
 
 /**
  * Listen for errors on all instances
@@ -19,43 +27,81 @@ Verb.on('generate.preInit', function(app) {
   });
 });
 
-Verb.on('verb.finished', function(app) {
-  app.emit('done');
-  process.exit();
+/**
+ * Initialize CLI
+ */
+
+Verb.on('generate.postInit', function(app) {
+  if (app.macros.has(args)) {
+    app.macros.set(args);
+    var macro = {};
+    macro[args[0]] = args.slice(2).join(' ');
+    console.log('saved macro:', util.inspect(macro));
+    process.exit();
+  }
+
+  var idx = utils.firstIndex(args, ['-D', '--default']);
+  if (idx !== -1) {
+    var del = args.indexOf('--del') !== -1;
+    if (del) {
+      app.base.store.del('defaultTask');
+    } else {
+      args.splice(idx, 1);
+      app.base.store.set('defaultTask', args);
+    }
+  }
 });
 
 /**
- * Initialize verb CLI
+ * Initialize Runner
  */
 
-utils.runner(Verb, {name: 'verb'}, argv, function(err, app, ctx) {
-  if (err) {
-    console.log(err.stack);
-    process.exit(1);
-  }
+var options = {name: 'verb'};
 
-  app.on('error', function(err) {
-    console.log(err.stack);
-    process.exit(1);
-  });
+utils.runner(Verb, options, argv, function(err, app, runnerContext) {
+  if (err) handleErr(app, err);
 
-  commands(app, ctx);
-  var config = app.base.get('cache.config') || {};
+  app.set('cache.runnerContext', runnerContext);
+  commands(app, runnerContext);
 
   if (!app.generators.defaults) {
     app.register('defaults', require('../lib/generator'));
   }
 
-  app.config.process(config, function(err, config) {
-    if (err) app.emit('error', err);
+  var ctx = utils.extend({}, runnerContext);
+  var config = app.get('cache.config') || {};
+  ctx.argv.tasks = [];
 
-    app.base.del('cache.config');
-    app.base.set('cache.config', config);
+
+  app.config.process(config, function(err, config) {
+    if (err) return handleErr(app, err);
+
+    app.base.cache.config = config;
 
     app.cli.process(ctx.argv, function(err) {
-      if (err) app.emit('error', err);
-      Verb.emit('verb.finished', app);
+      if (err) return handleErr(app, err);
+
+      var arr = tasks(app, ctx, argv);
+      app.log.success('running tasks:', arr);
+      app.generate(arr, function(err) {
+        if (err) return handleErr(app, err);
+
+        app.emit('done');
+        process.exit();
+      });
     });
   });
 });
 
+/**
+ * Handle errors
+ */
+
+function handleErr(app, err) {
+  if (app && app.base.hasListeners('error')) {
+    app.base.emit('error', err);
+  } else {
+    console.log(err.stack);
+    process.exit(1);
+  }
+}
