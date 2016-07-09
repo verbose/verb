54b4daafa97e54eb6d716e4e7ec0a901c39dce55
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-09 00:41:26 -0400

    update verbfile

--- verbfile.js
+++ verbfile.js
@@ -1,16 +1,28 @@
 'use strict';
 
 var through = require('through2');
+var reflinks = require('gulp-reflinks');
+var format = require('gulp-format-md');
+var utils = require('./lib/utils');
 
-module.exports = function(verb) {
-  verb.extendWith('verb-readme-generator');
+module.exports = function(app) {
+  app.use(require('verb-generate-readme'));
 
-  verb.task('default', ['readme'], function() {
-    return verb.src('README.md')
+  app.task('docs', ['setup'], function(cb) {
+    app.layouts('docs/src/templates/layouts/*.md');
+    return app.src('docs/src/*.md', {layout: 'default'})
+      .pipe(app.renderFile('*'))
+      .pipe(reflinks())
+      .pipe(format())
+      .pipe(app.dest('docs'))
+  });
+
+  app.task('default', ['readme'], function() {
+    return app.src('README.md')
       .pipe(through.obj(function(file, enc, next) {
         file.content = file.content.replace(/^(#{2,}\s*\[)\./gm, '$1');
         next(null, file);
       }))
-      .pipe(verb.dest('.'));
+      .pipe(app.dest('.'));
   });
 };
